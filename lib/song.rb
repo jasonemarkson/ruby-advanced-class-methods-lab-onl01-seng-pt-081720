2040321f15c298ba990b7082a46fb7be603761c0
Jason Markson <jasonmarkson76@gmail.com>
2020-09-08 01:04:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    split_filename = filename.chomp("mp3").split(" - ")
+    split_filename = filename.chomp(".mp3").split(" - ")
     song = self.new 
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
