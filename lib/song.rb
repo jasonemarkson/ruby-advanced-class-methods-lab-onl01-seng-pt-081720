6abdf632701f3a383e82d418d46306620fecc778
Jason Markson <jasonmarkson76@gmail.com>
2020-09-08 00:25:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if @@all.include(name)
+    if @@all.include?(name)
       self.find_by_name(name)
     else
       self.create_by_name(name)
